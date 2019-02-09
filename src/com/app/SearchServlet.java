bcd1bb822b4627909622e9656456059b909cfb35
Admin <Admin@192.168.1.10>
2019-02-09 19:36:11 +0530

    hi welcome to demo project

---
+++ src/com/app/SearchServlet.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class SearchServlet extends GenericServlet{
+	
+	public void service(ServletRequest req,ServletResponse resp) {
+		
+	}
+
+}
